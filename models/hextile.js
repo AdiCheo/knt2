7cb35ef3351fd7041f818ebb27f5cde44f61700e
Ibrahim <IbrahimManssouri@cmail.carleton.ca>
2014-03-31 21:49:40 -0400

    Hextile

--- models/hextile.js
+++ models/hextile.js
@@ -1,17 +1,6 @@
 //initialize a regular hexagon with all additional parameters and methods
 function HexTile(realX, realY, hexRadius, strokeColor, logicalX, logicalY) {
     return {
-      x: realX,
-      y: realY,
-      sides: 6,
-      radius: hexRadius,
-      stroke: strokeColor,
-      strokeWidth: 1,
-      name: 'hex',
-      rotation: 100,
-      fillPatternOffset: [150, 150 - hexRadius / 4],
-      fillPatternScale: [1 / 2, 1 / 2],
-      fillPatternRotation: -100,
       id: logicalX + "," + logicalY,
       logX: logicalX,
       logY: logicalY,
